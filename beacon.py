b60227da1c773b1602cca16b4833da5d815e4d4b
Emanuele Munafò <ema.muna95@gmail.com>
2018-03-15 13:07:30 +0100

    .

--- beacon.py
+++ beacon.py
@@ -270,7 +270,10 @@ class BeaconPi(object):
                     10 + report_data_length + 1])
                 report["payload"] = self.packet_as_hex_string(
                     report["payload_binary"], True, True)
-
+                    # Parse the payload
+                    print("\tMAJOR: ", self.printpacket(pkt[report_pkt_offset - 6: report_pkt_offset - 4]))
+                    print("\tMINOR: ", self.printpacket(pkt[report_pkt_offset - 4: report_pkt_offset - 2]))
+                    print("\tMAC address: ", self.packed_bdaddr_to_string(pkt[report_pkt_offset + 3:report_pkt_offset + 9]))
             # Each report length is (2 (event type, bdaddr type) + 6 (the address)
             #    + 1 (data length field) + data length + 1 (rssi)) bytes long.
             report_pkt_offset = report_pkt_offset + 10 + report_data_length + 1
@@ -335,11 +338,6 @@ class BeaconPi(object):
                     print("Raw Advertising Packet:")
                     print(self.packet_as_hex_string(pkt, True, True))
                     print("")
-                    report_pkt_offset = 0
-                    print("\tUDID: ", self.printpacket(pkt[report_pkt_offset - 22: report_pkt_offset - 6]))
-                    print("\tMAJOR: ", self.printpacket(pkt[report_pkt_offset - 6: report_pkt_offset - 4]))
-                    print("\tMINOR: ", self.printpacket(pkt[report_pkt_offset - 4: report_pkt_offset - 2]))
-                    print("\tMAC address: ", self.packed_bdaddr_to_string(pkt[report_pkt_offset + 3:report_pkt_offset + 9]))
                     # commented out - don't know what this byte is.  It's NOT TXPower
                     txpower, = struct.unpack("b", bytes([pkt[report_pkt_offset - 2]]))
                     print("\t(Unknown):", txpower)
