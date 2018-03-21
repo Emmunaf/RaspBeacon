9447d52cb35bfe5d0d0eb3751b6d6ed1c6836533
Emanuele Munafò <ema.muna95@gmail.com>
2018-03-21 15:16:06 +0100

    Fixed typo

--- beacon.py
+++ beacon.py
@@ -131,11 +131,11 @@ class BeaconPi(object):
         """Set the parameters needed for a (quick/slow) scan"""
 
         advertising_interval_min = 0x0100  # Minimum advertising interval for undirected and low duty cycle directed advertising. 
-        advertising_interval_min = 0x0120  # Maximum advertising interval, Range: 0x0020 to 0x4000|Default: N = 0x0800 (1.28 s)|Time = N * 0.625 ms|Time Range: 20 ms to 10.24 s
+        advertising_interval_max = 0x0120  # Maximum advertising interval, Range: 0x0020 to 0x4000|Default: N = 0x0800 (1.28 s)|Time = N * 0.625 ms|Time Range: 20 ms to 10.24 s
         advertising_type = ADV_NONCONN_IND  # Advertising Type([un]Connactable/[un]directed/...)
         own_address_type = 0x00  # 0x00 public, 0x01 random
         peer_address_type = 0x00
-        cmd_pkt = struct.pack("<HHBBB", advertising_interval_min, advertising_interval_min, advertising_type, own_address_type,
+        cmd_pkt = struct.pack("<HHBBB", advertising_interval_min, advertising_interval_max, advertising_type, own_address_type,
                               peer_address_type)  # LittleEndian(unsigned char, unsigned char, ..)
         print(cmd_pkt.hex())
         res = bluez.hci_send_cmd(self.hci_sock, OGF_LE_CTL, OCF_LE_SET_ADVERTISING_PARAMETERS, cmd_pkt)
