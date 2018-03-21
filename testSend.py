01be92720308c90dafac7ed06d91b93676bf7d1f
Emanuele Munafò <ema.muna95@gmail.com>
2018-03-21 14:43:32 +0100

    improved send test

--- testSend.py
+++ testSend.py
@@ -4,6 +4,7 @@ sock = a.open_socket()
 print("Le advertising test")
 while True:
     a.send_ack(0, 0)
+    a.hci_set_advertising_parameters()
     a.le_set_advertising_status(enable=True)
     #a.parse_events(5)
     
