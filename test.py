f6d37e63fe5f64568319dac9df7a41dfb3c151e8
Emanuele Munafò <ema.muna95@gmail.com>
2018-03-21 15:25:31 +0100

    Fixed tests

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ from beacon import BeaconPi
 a = BeaconPi()
 sock = a.open_socket()
 a.hci_le_set_scan_parameters()
-a.hci_set_advertising_parameters()
 a.start_le_scan()
+a.hci_set_advertising_parameters()
+a.le_set_advertising_status(enable=True)
 print("Le scan enabled")
 while True:
     a.send_ack(0, 0)
-    a.le_set_advertising_status(enable=True)
     a.parse_events(5)
     
 
--- testSend.py
+++ testSend.py
@@ -2,10 +2,10 @@ from beacon import BeaconPi
 a = BeaconPi()
 sock = a.open_socket()
 print("Le advertising test")
+a.hci_set_advertising_parameters()
+a.le_set_advertising_status(enable=True)
 while True:
     a.send_ack(0, 0)
-    a.hci_set_advertising_parameters()
-    a.le_set_advertising_status(enable=True)
     #a.parse_events(5)
     
 
